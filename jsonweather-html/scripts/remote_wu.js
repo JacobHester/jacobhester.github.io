b7a1e856a7f35f6ace35655dd5c967fc33173f8a
JacobHester <Lazjhester@gmail.com>
2016-11-22 17:07:11 -0700

    yeah buddy

--- jsonweather-html/scripts/remote_wu.js
+++ jsonweather-html/scripts/remote_wu.js
@@ -32,14 +32,15 @@ console.log(parsed_json);
   var temp_f = parsed_json['current_observation']['temp_f'];
     var summary = parsed_json['current_observation']['weather'];
 var humidity = parsed_json['current_observation']['relative_humidity'];
-    var celc = parsed_json['current_observations']['temp_c'];
-  
+  var celcius = parsed_json['current_observation']['temp_c'];
+    var wind = parsed_json['current_observation']['wind_mph'];
       
       $("#cityDisplay").text(location + " " + state);
       $("#summary").text(summary);
       $('#currentTemp').text(temp_f);
       $('#add1').text(humidity + "Humidity");
-      $('#add2').text(celc);
+      $('#add2').text(celcius + " " + "Degrees Celsius");
+      $('#add3').text(wind + " " + "mph");
       
       
 
